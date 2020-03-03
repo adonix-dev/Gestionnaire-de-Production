03e4014790c4def72ee62a7cb44aa35da21e029e
Antony Laget <antony.laget@icloud.com>
2020-03-03 10:46:22 +0100

    Merge branch 'antony'
    # Conflicts:
    #	projet-java-s6/src/main/java/org/antogautjean/App.java
    #	projet-java-s6/src/main/java/org/antogautjean/files/chaines.csv

--- projet-java-s6/src/main/java/org/antogautjean/GUI_test.java
+++ projet-java-s6/src/main/java/org/antogautjean/GUI_test.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class GUI extends Application {
+public class GUI_test extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
